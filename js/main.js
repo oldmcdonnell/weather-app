77e8418e2fbc0dd3b0e9b6dd2eeaf3824732f29b
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-10 21:10:40 -0400

    input text working

--- js/main.js
+++ js/main.js
@@ -1,26 +1,71 @@
-import { Axios } from "axios"
+import axios from "axios"
+const zipInput = document.getElementById("zipInput2");
+const log = document.getElementById("submit")
+//zipInput.addEventListener("change", updateZip)
+let zipCode = "40502"
+log.addEventListener("click", updateZip)
+
+function updateZip() {
+    //zipInput.value = e.target.value;
+ console.log("submit")
+ console.log(zipInput.value)
+getWeather(zipInput.value)
+}
 
 
-async function getWeatherFromAPI() {
+/*-
+if(zipInput.value.length === 5){
+    zipCode = zipInput.value;
+}
+-*/
+
+//const zipCode = "40502"
+
+//const staticLink = `https://api.openweathermap.org/data/2.5/weather?zip=40502,us&appid=40bc842ca745787e7b42c27345fb9e4a`
+
+// ${zipCode}
+async function getWeatherFromAPI(zipCode) {
+    const openWeatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=40bc842ca745787e7b42c27345fb9e4a`
     try {
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=40502,us&appid=40bc842ca745787e7b42c27345fb9e4a')
-        const weatherInfo = await response.json()
-        return weatherInfo
+        const response = await axios.get(openWeatherLink)
+        return response
+        // const weatherInfo = await response
+        // return weatherInfo
+        console.log("get success")
     } catch (error){
         console.log('Error: ', error)
         return []
     }
 }
 
-async function getWeather() {
-    const weatherInfo = await getWeatherFromAPI()
+async function getWeather(zipCode) {
+    const weatherInfo = await getWeatherFromAPI(zipCode)
     console.log('Weather ', weatherInfo)
+    console.log(weatherInfo.data.main.feels_like)
+    let currentTemp = weatherInfo.data.main.feels_like
 }
 
-getWeather()
+//getWeather();
 
 
 /*-
+function limit(element, max_chars)
+{
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0, max_chars);
+    }
+}
+function minmax(value, min, max) 
+{
+    if(parseInt(value) < min || isNaN(parseInt(value))) 
+        return 0; 
+    else if(parseInt(value) > max) 
+        return 100; 
+    else return value;
+}
+
+
+
 //https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
 
 'https://api.openweathermap.org/data/2.5/weather?zip=40502,us&appid=40bc842ca745787e7b42c27345fb9e4a'
@@ -35,4 +80,25 @@ https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API 
 
 
 api key 40bc842ca745787e7b42c27345fb9e4a
+
+
+/// input text 
+
+
+<input type="number" placeholder="Zipcode" id="zipCode" onkeydown="limit(this, 6);" onkeyup="limit(this, 6);" onkeyup="this.value = minmax(this.value, 0, 6)" required>
+
+function limit(element, max_chars)
+{
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0, max_chars);
+    }
+}
+function minmax(value, min, max) 
+{
+    if(parseInt(value) < min || isNaN(parseInt(value))) 
+        return 0; 
+    else if(parseInt(value) > max) 
+        return 100; 
+    else return value;
+}
 -*/
